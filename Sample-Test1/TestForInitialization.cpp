02fbc634723c419f8d97f49635315607588e3dc5
lsy <123456897>
2024-12-15 11:12:23 +0800

    Remove for update

--- Sample-Test1/TestForInitialization.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include"./Executor.h"
-using namespace adas;
-TEST(ExecutorTest, Initialization) {
-    Executor executor = Executor(0, 0, Direction::North);
-    // 初始位置应该是 (0, 0)，朝向是北
-    EXPECT_EQ(executor.GetPosition(), std::make_pair(0, 0));
-    EXPECT_EQ(executor.GetHeading(), "North");
-}
--- Sample-Test1/TestForWrongCase.cpp
+++
@@ -1,9 +0,0 @@
-#include"pch.h"
-#include"./Executor.h"
-using namespace adas;
-// 测试无效指令
-TEST(ExecutorTest, InvalidCommand) {
-    Executor executor = Executor(0, 0, Direction::North);
-    // 'X' 是无效指令，应该抛出 std::invalid_argument 异常
-    EXPECT_THROW(executor.ExecuteCommands("X"), std::invalid_argument);
-}
